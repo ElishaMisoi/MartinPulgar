520bd36405ce7eccd261416cf1827d1fe100fc2b
Elisha Misoi <45200891+ElishaMisoi@users.noreply.github.com>
2021-05-12 03:04:30 +0300

    Create BorderlessEntry.cs

---
+++ MartinPulgar.iOS/CustomControls/BorderlessEntry.cs
@@ -0,0 +1,22 @@
+﻿using MartinPulgar.CustomControls;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(BorderlessEntry), typeof(MartinPulgar.iOS.CustomControls.BorderlessEntry))]
+namespace MartinPulgar.iOS.CustomControls
+{
+    public class BorderlessEntry : EntryRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (Control != null)
+            {
+                Control.Layer.BorderWidth = 0;
+                Control.BorderStyle = UIKit.UITextBorderStyle.None;
+            }
+        }
+    }
+}
