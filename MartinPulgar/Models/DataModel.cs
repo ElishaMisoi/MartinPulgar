0c5266d7a6b1496a3017d9b074fff26a89ebfaf6
Elisha Misoi <45200891+ElishaMisoi@users.noreply.github.com>
2021-05-12 03:04:46 +0300

    Create DataModel.cs

---
+++ MartinPulgar/Models/DataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MartinPulgar.Models
+{
+    public class DataModel
+    {
+        public List<string> Images { get; set; }
+        public string Comments { get; set; }
+        public DateTime Date { get; set; }
+        public string Area { get; set; }
+        public string Category {get; set;}
+        public string Tags { get; set; }
+        public string Event { get; set; }
+    }
+}
