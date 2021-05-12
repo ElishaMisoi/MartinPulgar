cd94a9ca06c30db9ec5ad9fbdafba7cd83d03e57
Elisha Misoi <45200891+ElishaMisoi@users.noreply.github.com>
2021-05-12 03:04:59 +0300

    Create ImageToBase64Converter.cs

---
+++ MartinPulgar/Utils/ImageToBase64Converter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartinPulgar.Utils
+{
+    public static class ImageToBase64Converter
+    {
+        public static async Task<string> ConvertToBase64String(string filePath)
+        {
+            string base64String = string.Empty;
+            await Task.Run(() => 
+            {
+                FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                byte[] ImageData = new byte[fs.Length];
+                fs.Read(ImageData, 0, System.Convert.ToInt32(fs.Length));
+                fs.Close();
+                base64String = Convert.ToBase64String(ImageData);
+            });
+            return await Task.FromResult(base64String);
+        }
+    }
+}
